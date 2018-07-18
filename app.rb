9253376835701b75d3620cdfa6ac1aacf8ef75a0
alexagtn2714 <alexagaton@hotmail.com>
2018-07-18 13:39:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -39,9 +39,11 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do 
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    
-    @product = @number1 + @number2 
-  
+    @operation = params[:operation]
+    if @operation == "*"
+      @answer = @number1 * @number2 
+    if @operation == "+"
+      
     "The product is #{@product}."
   end 
 end
