122ae87a30a4ccf4f7bfe4e40acd309eb2d21808
alexagtn2714 <alexagaton@hotmail.com>
2018-07-18 13:46:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -46,8 +46,8 @@ class App < Sinatra::Base
       @answer = @number1 + @number2
     elsif @operation == "-"
       @answer = @number1 - @number2
-    elsif @operation == "/"
-      @answer = @number1 / @number2
+    # elsif @operation == "/"
+      # @answer = @number1 / @number2
    end
     "The answer is #{@answer}."
   end 
